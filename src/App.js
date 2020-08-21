63030128cd83eaff76f908805e79a9dd5749ceef
Engr Sabir Hussain <engineersabirhussain14@gmail.com>
2020-08-21 09:30:15 +0500

    updated text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Assalam O Alikum to all from us </h1>
+      <h1>Assalam O Alikum to all from us  </h1>
     </div>
   );
 }
